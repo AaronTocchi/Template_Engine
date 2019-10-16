c3f33a412050ddb0d4162f1fd3e9c90c021aaf59
Toki <actocchi@gmail.com>
2019-10-16 10:32:52 -0700

    prompt built for manager and employee

--- app.js
+++ app.js
@@ -1,6 +1,118 @@
+const inquirer = require("inquirer");
 const Engineer = require("./lib/engineer")
 const Intern = require("./lib/intern")
 const Manager = require("./lib/manager")
+
+
+// The getEmployee/getManager/getEnginner/getIntern functions return their response.
+function getEngineer() {
+
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What's your Engineer's name?",
+                name: "name"
+                // add some questions
+            },
+            {
+                type: "input",
+                message: "What's your Engineer's id?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What's your Engineer's email?",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Please provide your github account name.",
+                name: "github"
+            }
+        ])
+};
+
+function getIntern() {
+
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please provide your School name.",
+                name: "school"
+            }
+        ])
+};
+
+let team = []
+
+async function getData() {
+    let resolve;
+    let reject;
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What's your manager's name?",
+            name: "name"
+            // add some questions
+        },
+        {
+            type: "input",
+            message: "What's your manager's id?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What's your manager's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What's your manager's office number?",
+            name: "officeNumber"
+        }
+    ]).then(({ name, id, email, officeNumber }) => {
+        // still need to instantiate 
+        manager = new Manager(name, id, email, officeNumber);
+        team.push(manager)
+        getEmployee();
+        function getEmployee() {
+            inquirer.prompt([{
+                type: "list",
+                message: "What role would you like to add in your team?",
+                name: "role",
+                choices: ["Engineer", "Intern", "I don't want to add anymore"]
+            }]).then(data => {
+                if (data.role === 'Engineer') {
+                    getEngineer().then(({ name, id, email, github }) => {
+                        engineer = new Engineer(name, id, email, github)
+                        team.push(engineer)
+                        console.log(team)
+
+                        getEmployee();
+                    })
+
+                }
+                else if (data.role === 'Intern') {
+                    getIntern().then(({ name, id, email, school }) => {
+                        intern = new Intern(name, id, email, school)
+                        team.push(intern)
+                        
+                        getEmployee();
+                    })
+                }
+            })
+        }
+    })
+};
+
+
+getData();
+
+
+
 // prompt user to enter name id email + specific detail per role
 // first prompt manager
     // name
